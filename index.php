6f231e294c069a7f63e9c5370d80c1f993c39851
Susha Anastasiia <sushaanastacia@yandex.ru>
2022-02-07 17:00:58 +0200

    task2: Numbers

--- index.php
+++ index.php
@@ -3,4 +3,17 @@
 $very_bad_unclear_name = "15 chicken wings";
 $order = $very_bad_unclear_name;
 $order .= "and some spicy sauce";
-echo $very_bad_unclear_name;
+echo $very_bad_unclear_name . "\n";
+
+//task2: Numbers
+$digit = 5;
+echo $digit . "\n";
+
+$float_digit = 273.5;
+echo "float digit = " . $float_digit. "\n";
+
+echo 12 . "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo "the difference in spending money = " . ($last_month - $this_month) . "\n";
